9aee2cf9994fadbcfd007b5f5fd44512e0a7e2cd
sinau123 <sinau123@gmail.com>
2021-03-19 18:12:25 +0700

    fix nuxt config js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 // import { getCatDetailLinks } from './data/derivedCatData'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  // target: 'static',
-  ssr: false,
+  target: 'static',
+  ssr: true,
 
   generate: {
     crawler: true,
